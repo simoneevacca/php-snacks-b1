34ab2c13d4ec368877c5b6632821e74f7d55dcee
simoneevacca <v.simone.vacca@gmail.com>
2024-04-15 12:18:20 +0200

    add snack 1

---
+++ snack_1/index.php
@@ -0,0 +1,44 @@
+<!-- Creiamo un array contenente le partite di basket di un’ipotetica tappa del calendario. Ogni array avrà una squadra di casa e una squadra ospite, punti fatti dalla squadra di casa e punti fatti dalla squadra ospite. Stampiamo a schermo tutte le partite con questo schema:
+Olimpia Milano - Cantù | 55-60 -->
+<?php
+
+$matches = [
+     [
+        'team1' => 'Olimpia',
+        'team2' => 'Milano',
+        'pointTeam1' => 55,
+        'pointTeam2' => 60         
+     ],
+     [
+        'team1' => 'Olimpia',
+        'team2' => 'Milano',
+        'pointTeam1' => 55,
+        'pointTeam2' => 60         
+    ] ,
+    [
+        'team1' => 'Olimpia',
+        'team2' => 'Milano',
+        'pointTeam1' => 55,
+        'pointTeam2' => 60         
+    ]  
+    ];
+    
+
+foreach ($matches as $match) {
+    var_dump($match['team1'] . ' ' . $match['team2'] . ' ' . '|' . ' ' . $match['pointTeam1'] . ' ' . $match['pointTeam2']);
+};
+
+
+    
+?> 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
